24c1e15675f894ead6f67ef311965d2f214b845e
Moreno Gentili <moreno.gentili@codiceplastico.com>
2023-06-15 08:40:41 +0200

    Primo commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Timers;
+
+namespace TimersDemo
+{
+    internal class Program
+    {
+        private static Stopwatch stopwatch;
+        private static int counter = 0;
+        static void Main(string[] args)
+        {
+            System.Timers.Timer timer = new System.Timers.Timer
+            {
+                AutoReset = true,
+                Interval = 250
+            };
+
+            timer.Elapsed += OnElapsed;
+            stopwatch = Stopwatch.StartNew();
+            timer.Enabled = true;
+            Console.ReadLine();
+        }
+
+        private static void OnElapsed(object sender, ElapsedEventArgs e)
+        {
+            counter++;
+            var currentCounter = counter;
+            long elapsedMilliseconds = stopwatch.ElapsedMilliseconds;
+
+            if (counter % 5 == 0)
+            {
+                // simulo un'operazione più lenta delle altre
+                Thread.Sleep(300);
+            }
+
+            Console.WriteLine($"Esecuzione {currentCounter}: sono trascorsi {elapsedMilliseconds}ms dalla precedente esecuzione");
+            stopwatch.Restart();
+        }
+    }
+}
